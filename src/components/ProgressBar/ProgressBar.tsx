2f201e4caf4c76ffbb108c291fd1f4f4d6d07d5c
Philemon Forson <firstkoolaid21@gmail.com>
2024-01-24 22:16:19 +0000

    progress width updated

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -6,7 +6,7 @@ const ProgressBar: React.FC<{ progress: number; progressColor: string }> = ({
   progressColor,
 }) => {
   return (
-    <Box w={"80%"} h={"6px"} bgColor={"dark.800"} borderRadius={4}>
+    <Box w={"100%"} h={"6px"} bgColor={"dark.800"} borderRadius={4}>
       <Box w={`${progress}%`} bgColor={progressColor} h={"100%"} borderLeftRadius={4}></Box>
     </Box>
   );
